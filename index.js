3ffb9d8479ac5b365822eccd6f6aed1d3e076edb
shukri1236 <shukri1236>
2024-07-04 18:41:21 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+// Declare and assign values to variables
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';nodnp
